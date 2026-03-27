1474f80ab033e0cb23a0645e4dccf5a500f20c31
Rivan Stevanus <stevanustampi7@gmail.com>
2026-03-27 15:29:18 +0700

    dd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        ResetPassword::toMailUsing(function (object $notifiable, string $token) {
+            $url = url(route('password.reset', [
+                'token' => $token,
+                'email' => $notifiable->getEmailForPasswordReset(),
+            ], false));
+
+            return (new MailMessage)
+                ->subject('Pemberitahuan Reset Password - Elshaddai')
+                ->greeting('Shalom, ' . ($notifiable->nama_lengkap ?? 'Member') . '!')
+                ->line('Anda menerima email ini karena kami menerima permintaan untuk mengatur ulang (reset) kata sandi untuk akun Anda.')
+                ->action('Reset Kata Sandi', $url)
+                ->line('Tautan reset kata sandi ini akan kedaluwarsa dalam 60 menit.')
+                ->line('Jika Anda merasa tidak pernah meminta reset kata sandi, abaikan email ini dan pastikan akun Anda tetap aman.')
+                ->salutation('Tuhan Yesus Memberkati, Tim Elshaddai Learning Center');
+        });
     }
 }
